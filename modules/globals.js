f0d9b99c92fe5cd21ade4f53a64d32f70597d776
wfercanas <fernando.canas@outlook.com>
2022-04-30 21:30:44 -0500

    add/ checking globals

---
+++ modules/globals.js
@@ -0,0 +1,4 @@
+console.log(global); // Object[global]
+console.log(setInterval); // [Function: setInterval]
+console.log(process); // process {...}
+console.log(__dirname); // /home/fcanas/PlatziMaster/nodejs/modules
